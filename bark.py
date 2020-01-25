8da0bdfb3930540c4d02665e8521ca15239c68a6
ScottL <sjlteacher@gmail.com>
2020-01-25 18:53:56 -0500

    Added ability to import GitHub starred repos.

--- bark.py
+++ bark.py
@@ -59,6 +59,13 @@ def get_bookmark_id_for_deletion():
     return get_user_input('Enter a bookmark ID to delete')
 
 
+def get_github_import_options():
+    return {
+        'github_username': get_user_input('GitHub username'),
+        'preserve_timestamps': get_user_input('Preserve timestamps [Y/n]', required=False) in {'Y', 'y', None}
+    }
+
+
 def loop():
     options = {
         'A': Option('Add a bookmark', commands.AddBookmarkCommand(), prep_call=get_new_bookmark_data),
@@ -66,6 +73,7 @@ def loop():
         'T': Option('List bookmarks by title', commands.ListBookmarksCommand(order_by='title')),
         'D': Option('Delete a bookmark', commands.DeleteBookmarkCommand(), prep_call=get_bookmark_id_for_deletion),
         'Q': Option('Quit', commands.QuitCommand()),
+        'G': Option('Import GitHub stars', commands.ImportGitHubStarsCommand(), prep_call=get_github_import_options),
     }
 
     clear_screen()
